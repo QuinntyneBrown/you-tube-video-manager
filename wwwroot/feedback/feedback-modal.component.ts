78ef27fb6149e0582f0009d8f54042676a335d6f
Quinntyne Brown <quinntynebrown@gmail.com>
2016-03-19 13:47:46 -0400

    modal open and close

--- wwwroot/feedback/feedback-modal.component.ts
+++ wwwroot/feedback/feedback-modal.component.ts
@@ -1,5 +1,7 @@
 ﻿import { CanActivate, Component } from "../core/component-decorators";
 import { FeedbackActionCreator } from "./feedback.actions";
+import * as store from "../core/store";
+import * as modal from "../modal/modal.actions";
 
 require("./feedback-modal.component.css");
 
@@ -7,6 +9,12 @@ require("./feedback-modal.component.css");
     templateUrl: "wwwroot/feedback/feedback-modal.component.html",
     styleUrls: ["wwwroot/feedback/feedback-modal.component.css"],
     selector: "feedback-modal",
-    providers: []
+    providers: ["dispatcher"]
 })
-export class FeedbackModalComponent { }
+export class FeedbackModalComponent {
+    constructor(private dispatcher: store.IDispatcher) {
+
+    }
+
+    closeModal = () => this.dispatcher.dispatch(new modal.CloseModalAction());
+}
